0e4cad0ec1943f633f295d89e62a21b376130112
TeddyPeng <121505784+teddy135790196@users.noreply.github.com>
2025-05-24 16:11:18 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,10 @@
       <router-link to="/admin">管理頁面</router-link> |
       <router-link to="/login">登入</router-link> |
       <router-link to="/cart">購物車</router-link> |
-      <router-link to="/checkout-info">訂單資訊</router-link> |
-      <router-link to="/checkout-success">訂單成功</router-link> |
-      <router-link to="/register">加入會員</router-link>
+      <router-link to="/checkout-info">填寫訂單</router-link> |
+      <router-link to="/checkout-success">訂單確認</router-link> |
+      <router-link to="/register">加入會員</router-link> |
+      <router-link to="/order-query">查詢訂單</router-link>
     </nav>
 
     <!-- 這裡會顯示匹配的頁面元件 -->
