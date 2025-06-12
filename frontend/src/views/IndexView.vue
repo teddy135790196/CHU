81b56b76c9951b93badf66f35377c3b87bc2f2d9
ZhuoJunliang <kang900320@gmail.com>
2025-06-12 14:35:33 +0800

    首頁加頁首頁尾

--- frontend/src/views/IndexView.vue
+++ frontend/src/views/IndexView.vue
@@ -3,22 +3,37 @@
     <!-- 背景層 -->
     <!-- <div class="back-img"></div>  -->
 
+
+    <!-- 頁首區 -->
+    <div class="row">
+      <HeaderArea />
+    </div>
+
     <!-- <div class="container"> -->
       <!-- <div class="row"> -->
         <IndexPage />
       <!-- </div> -->
     <!-- </div> -->
+
+    <!-- 頁尾區 -->
+    <div class="row">
+      <FooterArea />
+    </div>
   </div>
 </template>
 
 <script>
 // 路徑要對應到 components 底下的元件位置
 import IndexPage from "@/components/pages/IndexPage.vue";
+import HeaderArea from "@/components/common/HeaderArea.vue";
+import FooterArea from "@/components/common/FooterArea.vue";
 
 export default {
   name: "IndexView",
   components: {
     IndexPage,
+    HeaderArea,
+    FooterArea,
   },
 };
 </script>
