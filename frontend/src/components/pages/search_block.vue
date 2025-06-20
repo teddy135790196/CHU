873aa5b847794404e3547fbb624bc4d366a558d5
EvaaaaaYO <106ac2018@gmail.com>
2025-06-20 17:48:21 +0800

    顯示作者簡介

--- frontend/src/components/pages/search_block.vue
+++ frontend/src/components/pages/search_block.vue
@@ -70,32 +70,36 @@ export default {
 
         // http://localhost:3000/api/products/search/con=author&kw=關鍵字
         //http://localhost:3000/api/products/author/作者
-        
+
         // 搜尋
         if (q || scope) {
           url += `search/con=${encodeURIComponent(scope)}&kw=${encodeURIComponent(q)}`;
           console.log("抓取的網址:", url);
           const response = await this.$axios.get(url);
           this.data = response.data;
-        this.books = this.data.books;
+          this.books = this.data.books;
         }
         else if (this.$route.name === 'author' && authorName) { //抓作者頁
           // this.$route.name是路由的路徑名稱(就是那個path:,name:...中的name)
           url += `author/${encodeURIComponent(authorName)}`;
           console.log("抓取的網址:", url);
-          const response = await this.$axios.get(url);this.data = response.data || {};
-        this.books = Array.isArray(this.data.books) ? this.data.books : [];
-        //取下作者的姓名、簡介並傳到page
-        if(this.books.length>0){
-          this.$emit('author_info',{
-            name:this.book[0].author,
-            introduce:this.books[0].introduce
-          });
-        }
+          const response = await this.$axios.get(url); this.data = response.data || {};
+          this.books = Array.isArray(this.data.books) ? this.data.books : [];
+          //取下作者的姓名、簡介並傳到page
+          // 一定要是陣列       且        陣列內容一定要有至少一本書
+          if (Array.isArray(this.books) && this.books.length > 0) {
+            // console.log("作者:",this.books[0].author);
+            // console.log("作者介紹:",this.books[0].introduce);
+            // if(this.books.length>0){
+            this.$emit('author_info', {
+              name: this.books[0].author,
+              introduce: this.books[0].introduce
+            });
+          }
         }
 
-        
-        
+
+
 
         // this.total = this.data.total;
       } catch (error) {
--- frontend/src/views/author_page.vue
+++ frontend/src/views/author_page.vue
@@ -4,14 +4,15 @@
       <sub_nav class="col-sm-2" />
       <div class="col-sm-10">
         <div class="card">
-  <div class="card-header">作者
-  </div>
-  <div class="card-body">
-    <p class="card-text">他是...</p>
-  </div>
-</div>
-        <search_block @select_isbn="getIsbn" @buy_isbn="buyIsbn"  /></div>
-      
+          <div class="card-header">{{ author }}
+          </div>
+          <div class="card-body">
+            <p class="card-text">{{ introduce }}</p>
+          </div>
+        </div>
+        <search_block @select_isbn="getIsbn" @buy_isbn="buyIsbn" @author_info="authorInfo" />
+      </div>
+
     </div>
   </div>
 </template>
@@ -22,32 +23,46 @@ import search_block from "@/components/pages/search_block.vue";
 // 輸出到外面
 export default {
   name: "author_page",
-  props:['isbn'],
-  components: { sub_nav,search_block },
-  
- methods:{
-     getIsbn(isbn) {
+  props: ['isbn'],
+  components: { sub_nav, search_block },
+  data() {
+    return {
+      //作者資訊
+      author: "",
+      introduce: ""
+    }
+  },
+  methods: {
+    //接收作者資訊的變數
+    authorInfo(info) {
+      this.author = info.name;
+      this.introduce = info.introduce;
+    },
+    getIsbn(isbn) {
       //取到這個isbn了我要跳轉到isbn頁! -->現在的路由要改成...
       console.log('選擇的ISBN:', isbn); // 這裡是接在前端路由後
       this.$router.push(`/book/${encodeURIComponent(isbn)}`);
     },
-    buyIsbn(isbn){
+    buyIsbn(isbn) {
       // 取到這個IBSN了我要放到購物車
-      console.log('選擇要買的ISBN:', isbn); 
+      console.log('選擇要買的ISBN:', isbn);
     }
-    
+
   }
 };
 </script>
 
 <style scoped>
-
 /* 設定寬度跟置中 */
 .container {
   margin: 0 auto;
   width: 1400px;
 }
-@media (max-width: 670px)
-{.container{width: 100%;
-margin-top: 20px;}}
+
+@media (max-width: 670px) {
+  .container {
+    width: 100%;
+    margin-top: 20px;
+  }
+}
 </style>
