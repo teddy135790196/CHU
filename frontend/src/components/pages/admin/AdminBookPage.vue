63fa6d27b4b90b06c40dec5ccb3bb0c85aee2783
EvaaaaaYO <106ac2018@gmail.com>
2025-06-27 13:12:25 +0800

    切換添加書本跟瀏覽書本

--- frontend/src/components/pages/admin/AdminBookPage.vue
+++ frontend/src/components/pages/admin/AdminBookPage.vue
@@ -1,33 +1,49 @@
 <template>
-<div class="container">
-    <BookInsertArea :books="books"/>
-<BookTablesArea :books="books"/>
-</div>
+    <div class="container">
+        <button @click="change = !change">{{ cha_text }}</button>
+        <KeepAlive>
+            <component :is="currentComponent" :books="books"></component>
+        </KeepAlive>
+        <!-- <BookInsertArea :books="books"/>
+<BookTablesArea :books="books"/> -->
+    </div>
 </template>
 <script>
 import BookTablesArea from '@/components/areas/admin/BookTablesArea.vue';
 import BookInsertArea from '@/components/areas/admin/BookInsertArea.vue';
 
-export default{
+export default {
     name: 'AdminBookPage',
-    components:{BookTablesArea,BookInsertArea},
-    data(){
-        return{books:[]}
-        
-    },mounted(){
+    components: { BookTablesArea, BookInsertArea },
+    data() {
+        return {
+            change: false,
+            books: []
+        }
+
+    }, mounted() {
         //元件掛載後自動抓資料
         this.fetchBooks();
-    },methods:{
-        async fetchBooks(){
-            try{
-                const products=
-                await this.$axios.get('/api/azureDB/products');
-                this.books=products.data;
-            }catch(e){
+    }, methods: {
+        async fetchBooks() {
+            try {
+                const products =
+                    await this.$axios.get('/api/azureDB/products');
+                this.books = products.data;
+            } catch (e) {
                 //發生錯誤時印出錯誤訊息到主控台
-                console.error('書本讀取錯誤:',e);
+                console.error('書本讀取錯誤:', e);
             }
         }
+    }, computed: {
+        currentComponent() {
+            if (this.change) {
+                return 'BookInsertArea'
+            } else {
+                return 'BookTablesArea'
+            }
+        },
+        cha_text() { return this.change ? '瀏覽書籍' : '加入新書'; }
     }
 }
 </script>
